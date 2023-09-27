b38893ac3a4ecc7ddff84805621b8f6ca0c92bc7
Hüseyin Çinibulak <huseyincnblk@gmail.com>
2023-09-28 00:54:29 +0300

    mainActivity completed

--- app/src/main/java/com/hsyncnblk/bottom_nav/MainActivity.kt
+++ app/src/main/java/com/hsyncnblk/bottom_nav/MainActivity.kt
@@ -2,11 +2,37 @@ package com.hsyncnblk.bottom_nav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.hsyncnblk.bottom_nav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_main->replaceFragment(HomeFragment())
+                R.id.nav_favorite->replaceFragment(FavoriteFragment())
+                R.id.nav_person->replaceFragment(PersonFragment())
+                R.id.nav_settings->replaceFragment(SettingsFragment())
+
+            }
+            true
+
+
+        }
+
+
+
+    }
+    private fun replaceFragment(fragment:Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView,fragment)
+        fragmentTransaction.commit()
     }
 }
